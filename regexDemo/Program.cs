25f87d97339ac15d0e19a0e769abee10944eb992
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-05 11:44:52 +0530

    [Tirupathi] Add. email verification

--- regexDemo/Program.cs
+++ regexDemo/Program.cs
@@ -9,6 +9,7 @@ namespace regexDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to validate pin and email");
             Console.Write("Enter PIN code : ");
             string input = Console.ReadLine();
             string sampleRegex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
@@ -23,6 +24,22 @@ namespace regexDemo
             {
                 Console.WriteLine("Invalid PIN");
             }
+
+            Console.WriteLine("Enter an Email : ");
+            String email = Console.ReadLine();
+
+            string pattern = @"^[a-z][a-z0-9]*([_+-.][a-z0-9]+)?@([a-z0-9]{1,}[.]){1,2}[a-z]{2,}$";
+            Regex reg = new Regex(pattern);
+
+            if (reg.IsMatch(email))
+            {
+                Console.WriteLine("Valid email\n");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email\n");
+            }
+
         }
     }
 }
